069c35d79edae4cde2a78f20feca764b0afa0795
tetreault <tetreault1@gmail.com>
2018-11-06 17:08:10 -0500

    commit for bug report for producible demo on viro repo -- cannot get ngrok starting

--- ViroDemo/js/ARViro3DObject.js
+++ ViroDemo/js/ARViro3DObject.js
@@ -1,46 +1,42 @@
 import React, { Component } from "react";
 import { StyleSheet } from "react-native";
-import { ViroARScene, ViroText, ViroConstants, Viro3DObject } from "react-viro";
+import {
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroAmbientLight,
+  Viro3DObject,
+  ViroMaterials
+} from "react-viro";
 
 export default class ARViro3DObject extends Component {
   constructor() {
     super();
-
-    this.state = {
-      text: "Initializing AR..."
-    };
-
-    this._onInitialized = this._onInitialized.bind(this);
   }
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized}>
+      <ViroARScene>
         <Viro3DObject
-          source={require("/js/res/Metal_Bottle_With_Sprayer_Cap_Generic/Metal_Bottle_With_Sprayer_Cap_Generic.fbx")}
-          resources={[
-            require("/js/res/Metal_Bottle_With_Sprayer_Cap_Generic/bottle_diffuse.png"),
-            require("/js/res/Metal_Bottle_With_Sprayer_Cap_Generic/bottle_reflection.png"),
-            require("/js/res/Metal_Bottle_With_Sprayer_Cap_Generic/plastic_bump.png")
-          ]}
-          position={[0.0, 0.0, -10]}
-          scale={[0.1, 0.1, 0.1]}
-          type="OBJ"
+          source={require("./res/Metal_Bottle_With_Sprayer_Cap_Generic_fbx/Metal_Bottle_With_Sprayer_Cap_Generic.vrx")}
+          scale={[0.03, 0.03, 0.03]}
+          position={[0, 0, -2]}
+          type="VRX"
+          materials="bottle"
         />
       </ViroARScene>
     );
   }
-
-  _onInitialized(state, reason) {
-    if (state == ViroConstants.TRACKING_NORMAL) {
-      this.setState({
-        text: "Hello World!"
-      });
-    } else if (state == ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
-    }
-  }
 }
+
+ViroMaterials.createMaterials({
+  bottle: {
+    lightingModel: "Lambert",
+    diffuseTexture: require("./res/Metal_Bottle_With_Sprayer_Cap_Generic_fbx/bottle_diffuse.png"),
+    normalTexture: require("./res/Metal_Bottle_With_Sprayer_Cap_Generic_fbx/bottle_reflection.png"),
+    roughnessTexture: require("./res/Metal_Bottle_With_Sprayer_Cap_Generic_fbx/plastic_bump.png")
+  }
+});
 
 const styles = StyleSheet.create({
   helloWorldTextStyle: {
