0ab1e2a13ec3fe5f92ba1fcabc10a61cd9d2a033
tetreault <tetreault1@gmail.com>
2018-11-08 09:33:27 -0500

    wrapped Viro3DObject in ViroARCamera so the model stays relative to its position in the AR camera as opposed to absolutely placed in the ViroARScene

--- Viro3dModelDemo/js/ARViro3DObject.js
+++ Viro3dModelDemo/js/ARViro3DObject.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {
   ViroARScene,
+  ViroARCamera,
   ViroAmbientLight,
   Viro3DObject,
   ViroMaterials
@@ -15,14 +16,16 @@ export default class ARViro3DObject extends Component {
     return (
       <ViroARScene>
         <ViroAmbientLight color="#ffffff" />
-        <Viro3DObject
-          source={require("./res/Metal_Bottle_With_Sprayer_Cap_Generic_fbx/Metal_Bottle_With_Sprayer_Cap_Generic.vrx")}
-          scale={[0.02, 0.02, 0.02]}
-          position={[0, -0.5, -0.5]}
-          rotation={[-75, 0, 0]}
-          type="VRX"
-          materials="bottle"
-        />
+        <ViroARCamera>
+          <Viro3DObject
+            source={require("./res/Metal_Bottle_With_Sprayer_Cap_Generic_fbx/Metal_Bottle_With_Sprayer_Cap_Generic.vrx")}
+            scale={[0.02, 0.02, 0.02]}
+            position={[0, -0.35, -0.5]}
+            rotation={[-75, 0, 0]}
+            type="VRX"
+            materials="bottle"
+          />
+        </ViroARCamera>
       </ViroARScene>
     );
   }
